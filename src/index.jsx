add4cc09813a0bf4fe4ce6566483685e5b70147d
DarBaala <joker.milan1997gmail.com>
2022-07-25 13:51:07 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
